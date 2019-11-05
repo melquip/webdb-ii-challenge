e5e70b07614effdd6e6bdcb7c35c9759919ca87e
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-05 13:11:31 +0000

    Added dotenv to config server environment variables

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const server = require('./server');
 const port = process.env.PORT || 4000;
-server.listen(port, (req, res) => {
+server.listen(port, () => {
   console.log(`Listening at port ${port}`);
 });
