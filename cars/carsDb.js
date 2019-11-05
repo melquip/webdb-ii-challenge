953070252b1a299ec7add83b24ee121e3eb1226f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-05 13:54:39 +0000

    Stretch: Added seeds, update and delete routes

--- cars/carsDb.js
+++ cars/carsDb.js
@@ -4,4 +4,6 @@ module.exports = {
   get: () => db('cars'),
   getById: (id) => db('cars').where({ id }),
   insert: (car) => db('cars').insert(car),
+  update: (id, car) => db('cars').where({ id }).update(car),
+  delete: (id) => db('cars').where({ id }).del(),
 }
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -27,14 +27,28 @@ router.post('/', async (req, res, next) => {
     next(error);
   }
 });
-/*
-router.put('/:id', (req, res, next) => {
-  res.status(200).json({ message: "ok" });
+
+router.put('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const { VIN, make, model, mileage, transmissionType, statusOfTitle } = req.body;
+  try {
+    const car = await Cars.update(id, { VIN, make, model, mileage, transmissionType, statusOfTitle });
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
 });
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({ message: "ok" });
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const car = await Cars.delete(id);
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
 });
-*/
+
 router.use((error, req, res, next) => {
   res.status(500).json({
     file: 'carsRouter',
