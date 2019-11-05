2e4a333f6980593c144f6c03862e92246c516c82
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-05 13:28:39 +0000

    Added db requests to each route method, created car

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -2,14 +2,30 @@ const express = require('express');
 const Cars = require('./carsDb');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({ message: "ok" });
+router.get('/', async (req, res, next) => {
+  try {
+    const carList = await Cars.get();
+    res.status(200).json(carList);
+  } catch (error) {
+    next(error);
+  }
 });
-router.get('/:id', (req, res, next) => {
-  res.status(200).json({ message: "ok" });
+router.get('/:id', async (req, res, next) => {
+  try {
+    const car = await Cars.getById(req.params.id);
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
 });
-router.post('/', (req, res, next) => {
-  res.status(200).json({ message: "ok" });
+router.post('/', async (req, res, next) => {
+  const { VIN, make, model, mileage, transmissionType, statusOfTitle } = req.body;
+  try {
+    const car = await Cars.insert({ VIN, make, model, mileage, transmissionType, statusOfTitle });
+    res.status(200).json(car);
+  } catch (error) {
+    next(error);
+  }
 });
 /*
 router.put('/:id', (req, res, next) => {
@@ -19,5 +35,13 @@ router.delete('/:id', (req, res, next) => {
   res.status(200).json({ message: "ok" });
 });
 */
+router.use((error, req, res, next) => {
+  res.status(500).json({
+    file: 'carsRouter',
+    method: req.method,
+    url: req.url,
+    message: error.message
+  })
+});
 
 module.exports = router;
