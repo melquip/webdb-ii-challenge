1288bc34ed45e4485ca5a84223f1a5dd4097b4da
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-05 13:10:10 +0000

    Finalized first migration

--- data/migrations/20191105125416_add-cars-table.js
+++ data/migrations/20191105125416_add-cars-table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', table => {
+    table.increments();
+    table.text('VIN').notNullable();
+    table.text('make').notNullable();
+    table.text('model').notNullable();
+    table.text('mileage').notNullable();
+    table.text('transmissionType');
+    table.text('statusOfTitle');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
